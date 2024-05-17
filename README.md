a8793683426237d4770ed261b237a2f74b103af9
Ishwor Pro OMG wow <dulalishan3@gmail.com>
2024-05-17 12:26:37 +0100

    add main file

---
+++ README.md
@@ -0,0 +1,1 @@
+A program that shows the use of ternary operator in Java.
---
+++ Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int a = 10;
+        String isEven = (a%2==0) ? ("is even.") : ("is odd.");
+        System.out.println("The string " + isEven); 
+    }
+}
