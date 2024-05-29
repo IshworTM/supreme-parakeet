db99ee65e973e62a38ac8c4c71048b8bf03cc17f
Ishwor Pro OMG wow <dulalishan3@gmail.com>
2024-05-29 13:16:44 +0545

    remove trailing whitespace

--- Ternary.java
+++ Ternary.java
@@ -2,7 +2,6 @@ public class Ternary {
     public static void main(String[] args) {
         int a = 10;
         String isEven = (a%2==0) ? ("is even.") : ("is odd.");
-        System.out.println("The string " + isEven); 
-        
+        System.out.println("The string " + isEven);
     }
 }
